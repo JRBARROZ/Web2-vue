a615c3cd8890db5514510743c0f2c97edb4b6ebc
jrbarroz <jrbarroz04@gmail.com>
2021-05-21 17:11:32 -0300

    finishing todo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="w-full max-w-lg m-auto p-4 text-gray-100 mt-8">
+  <div class="app w-full max-w-lg m-auto p-4 text-gray-100 mt-8">
     <HelloWorld />
   </div>
 </template>
 <script>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import './App.css'
   export default {
     name: 'Home',
     components: {
